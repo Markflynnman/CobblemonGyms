2ca35261ac0031d9ae35c54eb9f4b15f727c9884
Markflynnman <markflynnman1221@gmail.com>
2025-03-14 13:54:01 -0400

    Base working neoforge project

--- src/main/java/com/markflynnman/cobblemon_gyms/CobblemonGyms.java
+++ src/main/java/com/markflynnman/cobblemon_gyms/CobblemonGyms.java
@@ -1,138 +1,129 @@
 package com.markflynnman.cobblemon_gyms;
 
-import com.cobblemon.mod.common.api.Priority;
-import com.cobblemon.mod.common.api.events.CobblemonEvents;
-import com.cobblemon.mod.common.api.events.starter.StarterChosenEvent;
-import com.markflynnman.cobblemon_gyms.capabilities.PlayerBadgeCollection;
-import com.markflynnman.cobblemon_gyms.capabilities.PlayerBadgeCollectionProvider;
-import com.markflynnman.cobblemon_gyms.capabilities.PlayerStarterPokemon;
-import com.markflynnman.cobblemon_gyms.capabilities.PlayerStarterPokemonProvider;
-import com.markflynnman.cobblemon_gyms.commands.BadgeCollectionCommands;
-import com.markflynnman.cobblemon_gyms.commands.CobblemonGymsCommands;
-import com.markflynnman.cobblemon_gyms.items.GymBadges;
-import com.markflynnman.cobblemon_gyms.items.ModCreativeModTabs;
-import com.markflynnman.cobblemon_gyms.menus.ModMenuTypes;
-import com.markflynnman.cobblemon_gyms.network.CBadgeCollectionDataSyncPacket;
-import com.markflynnman.cobblemon_gyms.network.CStarterPokemonDataSyncPacket;
-import com.markflynnman.cobblemon_gyms.network.PacketHandler;
-import com.mojang.logging.LogUtils;
-import kotlin.Unit;
-import net.minecraft.resources.ResourceLocation;
-import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
-import net.minecraftforge.event.AttachCapabilitiesEvent;
-import net.minecraftforge.event.RegisterCommandsEvent;
-import net.minecraftforge.event.entity.EntityJoinLevelEvent;
-import net.minecraftforge.event.entity.player.PlayerEvent;
-import net.minecraftforge.eventbus.api.IEventBus;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.server.command.ConfigCommand;
 import org.slf4j.Logger;
 
+import com.mojang.logging.LogUtils;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.api.distmarker.Dist;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.bus.api.SubscribeEvent;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.config.ModConfig;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
+import net.neoforged.neoforge.event.server.ServerStartingEvent;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+// The value here should match an entry in the META-INF/neoforge.mods.toml file
 @Mod(CobblemonGyms.MODID)
 public class CobblemonGyms
 {
+    // Define mod id in a common place for everything to reference
     public static final String MODID = "cobblemon_gyms";
+    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
+    // Create a Deferred Register to hold Blocks which will all be registered under the "examplemod" namespace
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+    // Create a Deferred Register to hold Items which will all be registered under the "examplemod" namespace
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "examplemod" namespace
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
 
-    public CobblemonGyms()
+    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
+    public static final DeferredBlock<Block> EXAMPLE_BLOCK = BLOCKS.registerSimpleBlock("example_block", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
+    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
+    public static final DeferredItem<BlockItem> EXAMPLE_BLOCK_ITEM = ITEMS.registerSimpleBlockItem("example_block", EXAMPLE_BLOCK);
+
+    // Creates a new food item with the id "examplemod:example_id", nutrition 1 and saturation 2
+    public static final DeferredItem<Item> EXAMPLE_ITEM = ITEMS.registerSimpleItem("example_item", new Item.Properties().food(new FoodProperties.Builder()
+            .alwaysEdible().nutrition(1).saturationModifier(2f).build()));
+
+    // Creates a creative tab with the id "examplemod:example_tab" for the example item, that is placed after the combat tab
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> EXAMPLE_TAB = CREATIVE_MODE_TABS.register("example_tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.examplemod")) //The language key for the title of your CreativeModeTab
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> EXAMPLE_ITEM.get().getDefaultInstance())
+            .displayItems((parameters, output) -> {
+                output.accept(EXAMPLE_ITEM.get()); // Add the example item to the tab. For your own tabs, this method is preferred over the event
+            }).build());
+
+    // The constructor for the mod class is the first code that is run when your mod is loaded.
+    // FML will recognize some parameter types like IEventBus or ModContainer and pass them in automatically.
+    public CobblemonGyms(IEventBus modEventBus, ModContainer modContainer)
     {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
 
-        ModCreativeModTabs.register(modEventBus);
-        ModMenuTypes.register(modEventBus);
-        GymBadges.register(modEventBus);
+        // Register the Deferred Register to the mod event bus so blocks get registered
+        BLOCKS.register(modEventBus);
+        // Register the Deferred Register to the mod event bus so items get registered
+        ITEMS.register(modEventBus);
+        // Register the Deferred Register to the mod event bus so tabs get registered
+        CREATIVE_MODE_TABS.register(modEventBus);
 
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
+        // Register ourselves for server and other game events we are interested in.
+        // Note that this is necessary if and only if we want *this* class (ExampleMod) to respond directly to events.
+        // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
+        NeoForge.EVENT_BUS.register(this);
 
-        // Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
-        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.CONFIG_SPEC, "CobblemonGyms-common.toml");
+        // Register the item to a creative tab
+        modEventBus.addListener(this::addCreative);
 
-        if (ModList.get().isLoaded("cobblemon")) {
-            LOGGER.info("Cobblemon loaded.");
-            CobblemonEvents.STARTER_CHOSEN.subscribe(Priority.NORMAL, this::onStarterChosenEvent);
-        }
-        else {
-            LOGGER.info("Cobblemon not loaded.");
-        }
+        // Register our mod's ModConfigSpec so that FML can create and load the config file for us
+        modContainer.registerConfig(ModConfig.Type.COMMON, Config.CONFIG_SPEC);
     }
 
-    public Unit onStarterChosenEvent(StarterChosenEvent event) {
-        LOGGER.warn(String.valueOf(event.getPokemon().getSpecies()) + " : " + String.valueOf(event.getPokemon().getPrimaryType().getName()) + " : " + event.getPokemon().getSpecies().getNationalPokedexNumber());
-
-        event.getPlayer().getCapability(PlayerStarterPokemonProvider.PLAYER_STARTER_POKEMON).ifPresent(starterPokemon -> {
-            starterPokemon.setStarterPokemon(event.getPokemon());
-        });
-        return Unit.INSTANCE;
+    private void commonSetup(final FMLCommonSetupEvent event)
+    {
+        // Some common setup code
+        LOGGER.info("HELLO FROM COMMON SETUP");
     }
 
-    @Mod.EventBusSubscriber(modid = MODID)
-    public static class ModEvents {
-        @SubscribeEvent
-        public static void RegisterCommands(RegisterCommandsEvent event) {
-            new CobblemonGymsCommands(event.getDispatcher());
-            new BadgeCollectionCommands(event.getDispatcher());
+    // Add the example block item to the building blocks tab
+    private void addCreative(BuildCreativeModeTabContentsEvent event)
+    {
+        if (event.getTabKey() == CreativeModeTabs.BUILDING_BLOCKS)
+            event.accept(EXAMPLE_BLOCK_ITEM);
+    }
 
-            ConfigCommand.register(event.getDispatcher());
-        }
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event)
+    {
+        // Do something when the server starts
+        LOGGER.info("HELLO from server starting");
+    }
 
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @EventBusSubscriber(modid = MODID, bus = EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+    public static class ClientModEvents
+    {
         @SubscribeEvent
-        public static void onAttachCapabilitiesPlayer(AttachCapabilitiesEvent<Entity> event) {
-            if (event.getObject() instanceof Player) {
-                if (!event.getObject().getCapability(PlayerBadgeCollectionProvider.PLAYER_BADGE_COLLECTION).isPresent()) {
-                    event.addCapability(new ResourceLocation(CobblemonGyms.MODID, "badge_collection"), new PlayerBadgeCollectionProvider());
-                }
-                if (!event.getObject().getCapability(PlayerStarterPokemonProvider.PLAYER_STARTER_POKEMON).isPresent()) {
-                    event.addCapability(new ResourceLocation(CobblemonGyms.MODID, "starter_pokemon"), new PlayerStarterPokemonProvider());
-                }
-            }
-        }
-
-        @SubscribeEvent
-        public static void onPlayerCloned(PlayerEvent.Clone event) {
-            if (event.isWasDeath()) {
-                event.getOriginal().getCapability(PlayerBadgeCollectionProvider.PLAYER_BADGE_COLLECTION).ifPresent(oldStore -> {
-                    event.getOriginal().getCapability(PlayerBadgeCollectionProvider.PLAYER_BADGE_COLLECTION).ifPresent(newStore -> {
-                        newStore.copyFrom(oldStore);
-                    });
-                });
-                event.getOriginal().getCapability(PlayerStarterPokemonProvider.PLAYER_STARTER_POKEMON).ifPresent(oldStore -> {
-                    event.getOriginal().getCapability(PlayerStarterPokemonProvider.PLAYER_STARTER_POKEMON).ifPresent(newStore -> {
-                        newStore.copyFrom(oldStore);
-                    });
-                });
-            }
-        }
-
-        @SubscribeEvent
-        public static void onRegisterCapabilities(RegisterCapabilitiesEvent event) {
-            event.register(PlayerBadgeCollection.class);
-            event.register(PlayerStarterPokemon.class);
-        }
-
-        @SubscribeEvent
-        public static void onPlayerJoinWorld(EntityJoinLevelEvent event) {
-            if (!event.getLevel().isClientSide()) {
-                if (event.getEntity() instanceof ServerPlayer player) {
-                    player.getCapability(PlayerBadgeCollectionProvider.PLAYER_BADGE_COLLECTION).ifPresent(badgeCollection -> {
-                        PacketHandler.sendToPlayer(new CBadgeCollectionDataSyncPacket(badgeCollection.getBadgeCollection()), player);
-                    });
-                }
-                if (event.getEntity() instanceof ServerPlayer player) {
-                    player.getCapability(PlayerStarterPokemonProvider.PLAYER_STARTER_POKEMON).ifPresent(starterPokemon -> {
-                        PacketHandler.sendToPlayer(new CStarterPokemonDataSyncPacket(starterPokemon.getStarterPokemon(), starterPokemon.getStarterPokemonType(), starterPokemon.getStarterPokemonDex()), player);
-                    });
-                }
-            }
+        public static void onClientSetup(FMLClientSetupEvent event)
+        {
+            // Some client setup code
+            LOGGER.info("HELLO FROM CLIENT SETUP");
+            LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
         }
     }
 }
